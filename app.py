5bdcc77a75143493eea0961d387ba2018a911af2
Jean LE CHAUDELEC <jlechaudelec@gmail.com>
2024-06-25 11:32:22 +0200

    Pages programmes et programme_exos

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
      return render_template("Home.html")
+
+@app.route("/")
+def programmes():
+     return render_template("programmes.html")
